1015485f6002f7ee1ef486fd2cdecf5a9aaa59a7
Mykyta Uzlov <fullstackly@gmail.com>
2020-05-30 17:38:25 +0300

    reducing letter declarations to single place

--- translator/translator.go
+++ translator/translator.go
@@ -6,24 +6,27 @@ import (
 )
 
 const (
-	consonants = "B, C, D, F, G, H, J, K, L, M, N, P, Q, R, S, T, V, W, X, Z" +
-		"b, c, d, f, g, h, j, k, l, m, n, p, q, r, s, t, v, w, x, z"
-	vowels = "A, E, I, O, U, Y" +
-		" a, e, i, o, u, y"
+	consonants  = "B, C, D, F, G, H, J, K, L, M, N, P, Q, R, S, T, V, W, X, Z"
+	vowels      = "A, E, I, O, U, Y"
 	consEnging  = "ay"
 	vowelEnding = "yay"
+)
+
+var (
+	allConsonants = consonants + strings.ToLower(consonants)
+	allVowels     = vowels + strings.ToLower(vowels)
 )
 
 // Translate translates english text according to pig-latin rules
 func Translate(text string) string {
 	var buf bytes.Buffer
 
-	if strings.Contains(consonants, string(text[0])) {
+	if strings.Contains(allConsonants, string(text[0])) {
 		prefix, base := splitPrefix(text)
 		buf.WriteString(base)
 		buf.WriteString(prefix)
 		buf.WriteString(consEnging)
-	} else if strings.Contains(vowels, string(text[0])) {
+	} else if strings.Contains(allVowels, string(text[0])) {
 		buf.WriteString(text)
 		buf.WriteString(vowelEnding)
 	}
@@ -35,7 +38,7 @@ func splitPrefix(word string) (prefix, base string) {
 	var subBuf bytes.Buffer
 
 	for i, let := range word {
-		if strings.Contains(vowels, string(let)) {
+		if strings.Contains(allVowels, string(let)) {
 			prefix = subBuf.String()
 			base = word[i:]
 			return
