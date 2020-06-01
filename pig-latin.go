7f87986fc7e205784d30cbc2d057dab09e4855e6
Mykyta Uzlov <fullstackly@gmail.com>
2020-06-01 12:12:59 +0300

    manage dependencies in CLI part

--- pig-latin.go
+++ pig-latin.go
@@ -7,18 +7,17 @@ import (
 	"github.com/fullstackly/pig-latin/translator"
 )
 
-var (
-	text = flag.String("translate", "", "translates given string according to pig-latin")
-)
-
 func main() {
+	text := flag.String("translate", "", "translates given string according to pig-latin")
+
 	flag.Parse()
-	process()
+
+	process(*text)
 }
 
-func process() {
-	if len(*text) != 0 {
-		fmt.Println(translator.TranslateText(*text))
+func process(text string) {
+	if len(text) != 0 {
+		fmt.Println(translator.TranslateText(text))
 	} else {
 		fmt.Println("Enter command and text. \nEnter -h for help")
 	}
